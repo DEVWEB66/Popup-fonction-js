0c27da94a1eb76137b2fed68defbd693e856e140
sofia <108486515+DEVWEB66@users.noreply.github.com>
2023-01-24 14:44:26 +0100

    Add files via upload

---
+++ Popup + fonction/app.js
@@ -0,0 +1,6 @@
+//CHALLENGE CREER UN POPUP AVEC UNE FONCTION DIRE BONJOUR PRENNE PRENOM EN PARAMETRE ET AFFICHE BONJOUR + PRENOM
+
+function direBonjour(prenom) {
+  alert("Bonjour " + prenom);
+}
+direBonjour("cher developpeur web");
